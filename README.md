fa18d52969f5041fc9dcaba09d929e0c26911e74
emilioz <emilioz@za.velocitytrade.com>
2023-05-08 20:32:47 +0200

    refactor into a few fns

--- README.md
+++ README.md
@@ -1,16 +1,38 @@
 # nugget
 
-It's like nuget, but with more g.
+It's like nuget, but more g.
 
 Essentially a wrapper around the `dotnet` CLI to enable it to do the things it **should** do already.
 
 This is a WIP. Features:
 
 - [x] Interactive updating of nuget packages across multiple projects
-- [ ] Automatic updating of all nugets
-- [ ] Filter nugets by a prefix
+- [x] Automatic updating of all nugets
+- [x] Filter nugets by a prefix
 - [ ] Make terminal output a little more palateable
 
 I'm too lazy to publish this on crates.io just yet. To install, `git clone https://github.com/emilioziniades/nugget && cd nugget && cargo install --path .`.
 
-To use, ensure you are in the folder containing the dotnet project whose nugets you would like to update, and run `nugget`. This will trigger an interactive process where you can select which nugets to update.
+To use, ensure you are in the folder containing the dotnet project whose nugets you would like to update, and run `nugget`. This will trigger an interactive process where you can select which nugets to update. For more usage details, run `nugget --help`.
+
+## examples
+
+1. Interactively update all outdated dependencies:
+```
+nugget
+```
+
+2. Automatically update all outdated dependencies:
+```
+nugget --auto
+```
+
+3. Interactively update outdated dependencies with one of the specified prefixes:
+```
+nugget --prefixes Mongo Redis
+```
+
+4. Automatically update outdated dependencies with one of the specified prefixes:
+```
+nugget --auto --prefixes Mongo Redis
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use std::{collections::HashMap, io, process::Command};
+use std::{
+    collections::HashMap,
+    io::{stdin, stdout, Write},
+    process::{exit, Command},
+};
 
 /// command line tool for better nuget management
 #[derive(Parser, Debug)]
@@ -20,20 +24,124 @@ struct Dependency {
     latest: String,
 }
 
+impl From<&str> for Dependency {
+    // assumes a dependency is expressed as a line of the following form:
+    //              > ThePackage        1.0.11        1.0.11        1.0.14
+    //                ^ name              ^ requested   ^resolved   ^ latest
+    //  it is incredibly brittle and may break at any moment. Unfortunately
+    //  dotnet does not provide the data in any structured form such as JSON.
+    fn from(line: &str) -> Self {
+        let dependency_details = line.trim_start().replace('>', "");
+        let dependency_details: Vec<&str> = dependency_details.split_whitespace().collect();
+        Self {
+            name: dependency_details[0].to_string(),
+            requested: dependency_details[1].to_string(),
+            resolved: dependency_details[2].to_string(),
+            latest: dependency_details[3].to_string(),
+        }
+    }
+}
+
+enum Action {
+    Confirm,
+    Deny,
+    Quit,
+    Other,
+}
+
+impl Action {
+    fn from_stdin(prompt: &str) -> Self {
+        print!("{} ", prompt);
+        stdout().flush().unwrap();
+
+        let action = {
+            let mut action = String::new();
+            stdin().read_line(&mut action).unwrap();
+            action.trim().to_lowercase()
+        };
+
+        match action.as_str() {
+            "y" | "yes" => Self::Confirm,
+            "n" | "no" => Self::Deny,
+            "q" | "quit" => Self::Quit,
+            _ => Self::Other,
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
-    println!("{args:?}");
+
+    let outdated_dependencies = if !args.prefixes.is_empty() {
+        get_outdated_dependencies()
+            .into_iter()
+            .filter(|(dependency, _)| {
+                args.prefixes
+                    .iter()
+                    .any(|prefix| dependency.name.starts_with(prefix))
+            })
+            .collect()
+    } else {
+        get_outdated_dependencies()
+    };
+
+    for (dependency, projects) in outdated_dependencies {
+        // automatic install
+        if args.auto {
+            update_dependency(dependency, &projects);
+            continue;
+        }
+
+        //interactive install
+        let prompt = format!(
+            "update {} ({} -> {}): {:?}? (y, n, q)",
+            &dependency.name, dependency.resolved, dependency.latest, projects
+        );
+
+        loop {
+            match Action::from_stdin(&prompt) {
+                Action::Confirm => {
+                    println!("installing");
+                    update_dependency(dependency, &projects);
+                    break;
+                }
+                Action::Deny => {
+                    println!("not installing");
+                    break;
+                }
+                Action::Quit => {
+                    println!("quitting");
+                    return;
+                }
+                Action::Other => {
+                    println!("unrecognized input, try again");
+                    continue;
+                }
+            }
+        }
+    }
+
+    // restore once all upgrades have happened
+    let output = Command::new("dotnet").arg("restore").output().unwrap();
+    println!("{}", stringify_bytes(&output.stdout));
+}
+
+fn get_outdated_dependencies() -> HashMap<Dependency, Vec<String>> {
     let output = Command::new("dotnet")
         .arg("list")
         .arg("package")
         .arg("--outdated")
         .output()
         .unwrap();
-    let output = std::str::from_utf8(&output.stdout).unwrap();
-    println!("{}", output);
+
+    if !output.status.success() {
+        println!("{}", stringify_bytes(&output.stderr));
+        exit(output.status.code().unwrap())
+    }
+
+    let output = stringify_bytes(&output.stdout);
 
     let mut dependency_map = HashMap::new();
-
     let mut current_project: String = String::new();
 
     for line in output.lines() {
@@ -47,16 +155,8 @@ fn main() {
                 "Project `{}` has the following updates to its packages",
                 line.bytes()
             );
-        } else if line.trim_start().starts_with(">") {
-            let dependency_details = line.trim_start().replace('>', "");
-            let dependency_details: Vec<&str> = dependency_details.split_whitespace().collect();
-            let dependency = Dependency {
-                name: dependency_details[0].to_string(),
-                requested: dependency_details[1].to_string(),
-                resolved: dependency_details[2].to_string(),
-                latest: dependency_details[3].to_string(),
-            };
-
+        } else if line.contains(">") {
+            let dependency = Dependency::from(line);
             dependency_map
                 .entry(dependency)
                 .or_insert(vec![])
@@ -64,47 +164,23 @@ fn main() {
         }
     }
 
-    for (dependency, projects_used) in dependency_map {
-        println!(
-            "update {} ({}): {:?}? (y, n, q)",
-            dependency.name.clone(),
-            dependency.latest,
-            projects_used
-        );
+    dependency_map
+}
 
-        loop {
-            let mut decision = String::new();
-            io::stdin().read_line(&mut decision).unwrap();
-            match decision.as_str().trim() {
-                "y" => {
-                    println!("installing!");
-                    for project_used in projects_used {
-                        let output = Command::new("dotnet")
-                            .arg("add")
-                            .arg(project_used)
-                            .arg("package")
-                            .arg(dependency.name.clone())
-                            .output()
-                            .unwrap();
-                        println!("{output:?}");
-                        let output = std::str::from_utf8(&output.stdout).unwrap();
-                        println!("{}", output);
-                    }
-                    break;
-                }
-                "n" => {
-                    println!("not installing!");
-                    break;
-                }
-                "q" => {
-                    println!("quitting");
-                    return;
-                }
-                _ => {
-                    println!("unrecognized input");
-                    continue;
-                }
-            }
-        }
+fn update_dependency(dependency: Dependency, projects: &[String]) {
+    for project in projects {
+        let output = Command::new("dotnet")
+            .arg("add")
+            .arg(project)
+            .arg("package")
+            .arg(&dependency.name)
+            .arg("--no-restore") // skip restoring as dotnet will complain about package downgrades
+            .output()
+            .unwrap();
+        println!("{}", stringify_bytes(&output.stdout));
     }
+}
+
+fn stringify_bytes(bytes: &Vec<u8>) -> &str {
+    std::str::from_utf8(bytes).unwrap()
 }
